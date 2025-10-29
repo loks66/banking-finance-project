b4696e9bef4ce24a09769a694a8613cd6da26691
Loks_TG <laxmikantag903@gmail.com>
2025-10-29 13:11:22 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,8 +34,24 @@ resource "aws_security_group" "allow_all" {
   }
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "server" {
-  ami           = "ami-0522ab6e1ddcc7055"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   key_name = "key02"
   vpc_security_group_ids = [aws_security_group.allow_all.id]
@@ -43,6 +59,7 @@ resource "aws_instance" "server" {
   tags = {
     Name = "${terraform.workspace}_server"
   }
+}
   provisioner "remote-exec" {
     inline = [
       "cat /etc/os-release",
